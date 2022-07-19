4ef37f0681a5cf1140face5c354cdda9d68f6a05
Martin Clark <webdeveloperplug@gmail.com>
2022-07-18 23:12:06 -0500

    Martin Nightly Updates

---
+++ confirmation-page.js
@@ -0,0 +1,7 @@
+/*page control */
+const submitBtn = document.getElementById("submit");
+
+function confirmationPage() {
+    window.location.href = "confirmation-page.html";
+}
+addEventListener('click', confirmationPage);
